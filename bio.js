1ad71ccc80dcb54a778c7a8df7609f5915571a45
bwaterman8 <bwaterman8@gmail.com>
2020-07-20 17:03:04 -0400

    projects column transitions and fade almost complete

--- bio.js
+++ bio.js
@@ -1,3 +1,5 @@
+
+
 document.querySelector('.bio_column').addEventListener('click', function() {
     document.querySelector('.modal_back').style.display = 'flex';
 })
@@ -5,3 +7,11 @@ document.querySelector('.bio_column').addEventListener('click', function() {
 document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('.modal_back').style.display = 'none';
 })
+
+document.querySelector('.projects_column').addEventListener('mouseover', function() {
+    document.querySelector('.projects_list').style.display = 'flex';
+}) 
+
+document.querySelector('.projects_column').addEventListener('mouseout', function() {
+    document.querySelector('.projects_list').style.display = 'none';
+}) 
