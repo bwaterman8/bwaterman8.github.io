25c9e713dba7e179ea7033f7c90d0ad5906f238a
bwaterman8 <bwaterman8@gmail.com>
2020-07-20 14:58:50 -0400

    bio modal complete

---
+++ bio.js
@@ -0,0 +1,7 @@
+document.querySelector('.bio_column').addEventListener('click', function() {
+    document.querySelector('.modal_back').style.display = 'flex';
+})
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.modal_back').style.display = 'none';
+})
